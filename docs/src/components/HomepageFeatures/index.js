df43dea024a4af1bfee844067953d8d12090c715
Andy Edwards <jedwards@fastmail.com>
2023-05-26 16:14:07 -0500

    docs: add feature bullet points to homepage [ci skip]

--- docs/src/components/HomepageFeatures/index.js
+++ docs/src/components/HomepageFeatures/index.js
@@ -3,43 +3,43 @@ import clsx from "clsx";
 import styles from "./styles.module.css";
 
 const FeatureList = [
-  // {
-  //   title: 'Easy to Use',
-  //   Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-  //   description: (
-  //     <>
-  //       Docusaurus was designed from the ground up to be easily installed and
-  //       used to get your website up and running quickly.
-  //     </>
-  //   ),
-  // },
-  // {
-  //   title: 'Focus on What Matters',
-  //   Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-  //   description: (
-  //     <>
-  //       Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-  //       ahead and move your docs into the <code>docs</code> directory.
-  //     </>
-  //   ),
-  // },
-  // {
-  //   title: 'Powered by React',
-  //   Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-  //   description: (
-  //     <>
-  //       Extend or customize your website layout by reusing React. Docusaurus can
-  //       be extended while reusing the same header and footer.
-  //     </>
-  //   ),
-  // },
+  {
+    title: "End-to-end Type Safety",
+    // Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
+    description: (
+      <>
+        You declare the shape and behavior of your API in one place, and get a
+        typed frontend client without a build step.
+      </>
+    ),
+  },
+  {
+    title: "Batteries Included",
+    // Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
+    description: (
+      <>
+        Provides plugins for Next.js, NextAuth.js, and Prisma, and powerful
+        tools for implementing pagination, expansion, and field selection.
+      </>
+    ),
+  },
+  {
+    title: "Maintainable OpenAPI",
+    // Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
+    description: (
+      <>
+        Automatically generates an OpenAPI spec from your backend API
+        declarations.
+      </>
+    ),
+  },
 ];
 
 function Feature({ Svg, title, description }) {
   return (
     <div className={clsx("col col--4")}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        {/* <Svg className={styles.featureSvg} role="img" /> */}
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
