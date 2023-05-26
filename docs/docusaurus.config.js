2cf2cc82d507c3af4be596125f7cb3d06300f167
Alex Rattray <rattray.alex@gmail.com>
2023-05-26 17:57:00 -0400

    Warn on broken links

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   organizationName: "stainless-api", // Usually your GitHub org/user name.
   projectName: "stl-api", // Usually your repo name.
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
