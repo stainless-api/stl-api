9d22bbfec79cc4871e27aad1be9c6f2a8ecaf8b2
Andy Edwards <jedwards@fastmail.com>
2023-06-05 18:37:00 -0400

    chore(react-query): code cleanup

--- packages/react-query/src/util.ts
+++ packages/react-query/src/util.ts
@@ -1,37 +1,3 @@
-type ToLowerCase = {
-  A: "a";
-  B: "b";
-  C: "c";
-  D: "d";
-  E: "e";
-  F: "f";
-  G: "g";
-  H: "h";
-  I: "i";
-  J: "j";
-  K: "k";
-  L: "l";
-  M: "m";
-  N: "n";
-  O: "o";
-  P: "p";
-  Q: "q";
-  R: "r";
-  S: "s";
-  T: "t";
-  U: "u";
-  V: "v";
-  W: "w";
-  X: "x";
-  Y: "y";
-  Z: "z";
-};
-
-export type LowerFirst<S extends string> =
-  S extends `${infer L extends keyof ToLowerCase}${infer Rest}`
-    ? `${ToLowerCase[L]}${Rest}`
-    : S;
-
 type ToUpperCase = {
   a: "A";
   b: "B";
