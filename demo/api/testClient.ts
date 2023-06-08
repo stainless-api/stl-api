585e0c3e2024d330e1e8f38d20cfcdb4ee5a6d9a
Andy Edwards <jedwards@fastmail.com>
2023-06-08 16:37:23 -0400

    fix(client): throw error on 4xx/5xx

--- demo/api/testClient.ts
+++ demo/api/testClient.ts
@@ -2,9 +2,8 @@ import { createClient } from "stainless";
 import type { api } from "./api";
 import fetch from "node-fetch";
 
-export const testClient = createClient<typeof api>(
-  "http://localhost:3000/api",
-  {
-    fetch: fetch as any,
-  }
-);
+export const baseUrl = "http://localhost:3000/api";
+
+export const testClient = createClient<typeof api>(baseUrl, {
+  fetch: fetch as any,
+});
