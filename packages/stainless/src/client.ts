8b29f0b0b3d4f8cf6a6e4234a1b9be98b39828be
Darius Jankauskas <d5926j@gmail.com>
2023-06-12 14:18:50 -0400

    Add client documentation comments.

--- packages/stainless/src/client.ts
+++ packages/stainless/src/client.ts
@@ -30,6 +30,47 @@ type ExtractClientResponse<E extends AnyEndpoint> = z.infer<
   ? ClientPromise<z.infer<E["response"]>>
   : ClientPromise<undefined>;
 
+/**
+ * A client for making requests to the REST API described by the `Api` type.
+ * 
+ * The client allows for performing REST operations idiomatically by utilizing
+ * the resources and endpoints declared on the API. A resource's endpoints 
+ * are available on the client as methods on a field with the name of the 
+ * resource.
+ * 
+ * ## Example
+ * ```ts
+ * // api/users/index.ts 
+ * import { stl } from "~/libs/stl";
+ * import { retrieve } from "./retrieve";
+ * import { User } from "./models";
+ * 
+ * export const users = stl.resource({
+ *   summary: "Users",
+ *   models: {
+ *     User,
+ *   },
+ *   actions: {
+ *     retrieve,
+ *   },
+ * });
+ * 
+ * // app/users/[userId]/page.tsx
+ * ...
+ * // `users` refers to the resource by the same name, `retrieve` refers
+ * // to the endpoint on `users` by the same name
+ * const user = client.users.retrieve(userId);
+ * ...
+ * 
+ * ## Implementation note
+ * In order to decrease build times and bundle sizes, the 
+ * `stainless` client utilizes proxies in order to provide a mapping 
+ * to the server API. This means that resource and endpoint fields and methods
+ * cannot be found within source code. Autocomplete and type definitions
+ * within a Typescript LSP-enabled editor are good ways of understanding
+ * the shape of the client API.
+ * ```
+ */
 export type StainlessClient<Api extends AnyAPIDescription> = ClientResource<
   ResourceConfig<
     Api["topLevel"]["actions"],
@@ -77,15 +118,51 @@ function actionMethod(action: string): HttpMethod {
   return "post";
 }
 
+/**
+ * Thrown when a non-success HTTP status code is encountered by the client.
+ */
 class HTTPResponseError extends Error {
+  /**
+   * @param response the unsuccessful response
+   */
   constructor(public response: Response) {
     super(`HTTP Error Response: ${response.status} ${response.statusText}`);
   }
 }
 
+/** Options for the {@link createClient} function. */
+interface ClientOpts {
+  /** Replaces the default global `fetch` function to make requests. */
+  fetch?: typeof fetch;
+}
+
+/**
+ * Creates a client for making requests against a `stainless` framework API.
+ * For usage information see {@link StainlessClient}.
+ *
+ * @param baseUrl the url to prepend to all request paths 
+ * @param options options to customize client behavior
+ * @returns client
+ * 
+ * ## Example
+ * ```ts
+ * // ~/api/client.ts
+ * import { createClient } from "stainless";
+ * import type { api } from "./index";
+ * 
+ * export const client = createClient<typeof api>("/api");
+ * ```
+ * 
+ * ## Client-server separation
+ * In order to get a client with correct typing information, it's necessary
+ * to import the type of the API from server-side code. This does not expose 
+ * server-side code or secrets to users because types are transpile-time 
+ * constructs. Importing values or functions from server-side code, however, 
+ * would result in this being included in client-side bundles. 
+ */
 export function createClient<Api extends AnyAPIDescription>(
   baseUrl: string,
-  options?: { fetch?: typeof fetch }
+  options?: ClientOpts
 ): StainlessClient<Api> {
   const checkStatus = (response: Response) => {
     if (response.ok) {
@@ -184,19 +261,39 @@ export function createClient<Api extends AnyAPIDescription>(
   return client;
 }
 
+/** A record of HTTP header names and values. */
 export type Headers = Record<string, string | null | undefined>;
 export type KeysEnum<T> = { [P in keyof Required<T>]: true };
 
+/** 
+ * Request options to customize the behavior of a 
+ * {@link StainlessClient} request. 
+ */
+
+// TODO most of these are unimplemented
 export type RequestOptions<Req extends {} = Record<string, unknown>> = {
+  /** Override the method with which to perform the request. */
   method?: HttpMethod;
+  /** Override the path at which to make the request. */
   path?: string;
+  /** Provide query parameters for the request. */
   query?: Req | undefined;
+  /** Provide body parameters for the request. */
   body?: Req | undefined;
+  /** Provide HTTP headers to send with the request. */
   headers?: Headers | undefined;
 
+  /** The maximum number of times to retry a request. Defaults to 0. */
   maxRetries?: number;
+  /** Whether to stream the response. */
   stream?: boolean | undefined;
+  /** The number of milliseconds before the request times out. */
   timeout?: number;
+  /**
+   * Make the request idempotent. Assuming server-side support,
+   * if the request is performed multiple times, the requested action will
+   * be performed at most once for a given value of the key.
+   */
   idempotencyKey?: string;
 };
 
@@ -216,7 +313,7 @@ const requestOptionsKeys: KeysEnum<RequestOptions> = {
   idempotencyKey: true,
 };
 
-export const isRequestOptions = (obj: unknown): obj is RequestOptions => {
+const isRequestOptions = (obj: unknown): obj is RequestOptions => {
   return (
     typeof obj === "object" &&
     obj !== null &&
@@ -225,6 +322,7 @@ export const isRequestOptions = (obj: unknown): obj is RequestOptions => {
   );
 };
 
+/** Fetched page data, along with associated pagination state. */
 export type Page<D extends z.PageData<any>> = PageImpl<D> & D;
 
 export class PageImpl<D extends z.PageData<any>> {
@@ -238,12 +336,24 @@ export class PageImpl<D extends z.PageData<any>> {
     Object.assign(this, data);
   }
 
+  /** The items in a page. */
   declare items: z.PageItemType<D>[];
+  /** The cursor referring to the first element in the page, if present. */
   declare startCursor: string | null;
+  /** 
+   * The cursor referring to the one past the last element in the page, 
+   * if present. 
+   */
   declare endCursor: string | null;
+  /** If known, whether there is a page after the current one. */
   declare hasNextPage: boolean | undefined;
+  /** If known, whether there is a page before the current one. */
   declare hasPreviousPage: boolean | undefined;
 
+  /** 
+   * Get params to access previous page. 
+   * @throws if there is no `startCursor`. 
+   */
   getPreviousPageParams(): z.PaginationParams {
     const { startCursor } = this.data;
     if (startCursor == null) {
@@ -270,14 +380,23 @@ export class PageImpl<D extends z.PageData<any>> {
     };
   }
 
+  /** Gets the cursor to access the previous page. */
   getPreviousPageParam(): string | null {
     return this.data.startCursor;
   }
 
+  /**
+   * Gets the URL at which to access the previous page. 
+   * @throws if there is no `startCursor`. 
+   */
   getPreviousPageUrl(): string {
     return `${this.pathname}/${qs.stringify(this.getPreviousPageParams())}`;
   }
 
+  /**
+   * Gets the previous page.
+   * @throws if there is no `startCursor`. 
+   */
   async getPreviousPage(): Promise<Page<D>> {
     return await this.clientPath.reduce(
       (client: any, path) => client[path],
@@ -285,6 +404,10 @@ export class PageImpl<D extends z.PageData<any>> {
     )(this.getPreviousPageParams());
   }
 
+  /** 
+   * Get params to access the next page. 
+   * @throws if there is no `endCursor`. 
+   */
   getNextPageParams(): z.PaginationParams {
     const { endCursor } = this.data;
     if (endCursor == null) {
@@ -309,14 +432,23 @@ export class PageImpl<D extends z.PageData<any>> {
     };
   }
 
+  /** Gets the cursor to access the next page. */
   getNextPageParam(): string | null {
     return this.data.endCursor;
   }
 
+  /**
+   * Gets the URL at which to access the next page. 
+   * @throws if there is no `endCursor`. 
+   */
   getNextPageUrl(): string {
     return `${this.pathname}/${qs.stringify(this.getNextPageParams())}`;
   }
 
+  /**
+   * Gets the next page.
+   * @throws if there is no `endCursor`. 
+   */
   async getNextPage(): Promise<Page<D>> {
     return await this.clientPath.reduce(
       (client: any, path) => client[path],
@@ -334,6 +466,7 @@ type ClientPromiseProps = {
   cacheKey: string;
 };
 
+/** A promise returned by {@link StainlessClient} requests. */
 class ClientPromise<R> implements Promise<R> {
   fetch: () => Promise<R>;
   method: HttpMethod;
@@ -388,8 +521,8 @@ export type { ClientPromise };
 
 /**
  * The result of client.???.list, can be awaited like a
- * Promise to get a single page, or async iterated to go through
- * all items
+ * `Promise` to get a single page, or async iterated to go through
+ * all items.
  */
 class PaginatorPromise<D extends z.PageData<any>>
   extends ClientPromise<Page<D>>
