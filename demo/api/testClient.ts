7bfa5d738f0473553c87dc5bda57fe6bebe3853c
Andy Edwards <jedwards@fastmail.com>
2023-06-13 12:42:44 -0400

    ci: use 0.0.0.0 instead of localhost

--- demo/api/testClient.ts
+++ demo/api/testClient.ts
@@ -2,8 +2,8 @@ import { createClient } from "stainless";
 import type { api } from "./api";
 import fetch from "node-fetch";
 
-export const baseUrl = `http://localhost:${
-  process.env.CI ? "3005" : "3000"
+export const baseUrl = `http://${
+  process.env.CI ? "0.0.0.0:3005" : "localhost:3000"
 }/api`;
 
 export const testClient = createClient<typeof api>(baseUrl, {
