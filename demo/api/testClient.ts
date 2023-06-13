0ae58c8acf658b2725743c566a79f1df94f4af40
Andy Edwards <jedwards@fastmail.com>
2023-06-13 12:36:26 -0400

    ci: try a different test port in ci

--- demo/api/testClient.ts
+++ demo/api/testClient.ts
@@ -2,7 +2,9 @@ import { createClient } from "stainless";
 import type { api } from "./api";
 import fetch from "node-fetch";
 
-export const baseUrl = "http://localhost:3000/api";
+export const baseUrl = `http://localhost:${
+  process.env.CI ? "3005" : "3000"
+}/api`;
 
 export const testClient = createClient<typeof api>(baseUrl, {
   fetch: fetch as any,
