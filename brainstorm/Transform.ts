acda84e50f6050c7895795d6f48e350e63644fd2
Andy Edwards <jedwards@fastmail.com>
2023-06-15 10:34:48 -0400

    chore: more updates to Transform idea

--- brainstorm/Transform.ts
+++ brainstorm/Transform.ts
@@ -2,12 +2,30 @@ import z from "zod";
 
 export const TransformSymbol = Symbol("Transform");
 
-export abstract class Transform<O, I> {
-  [TransformSymbol] = true;
-  abstract transform(value: input<I>): output<O>;
+export abstract class SchemaType<O, I> {
+  declare _input: I;
+  declare _output: O;
 }
 
-export type input<T> = T extends Transform<any, infer I>
+export abstract class Transform<O, I> extends SchemaType<O, I> {
+  [TransformSymbol] = true;
+  abstract transform(value: output<I>): O | PromiseLike<O>;
+}
+
+export const RefineSymbol = Symbol("Refine");
+
+export abstract class Refine<I, O> extends Transform<O, I> {
+  [RefineSymbol] = true;
+  transform(value: output<I>): O {
+    if (!this.refine(value)) {
+      throw new Error("value is not the right type");
+    }
+    return value as O;
+  }
+  abstract refine(arg: output<I>): boolean;
+}
+
+export type input<T> = T extends SchemaType<any, infer I>
   ? input<I>
   : T extends Date
   ? Date
@@ -17,10 +35,10 @@ export type input<T> = T extends Transform<any, infer I>
   ? Array<input<E>>
   : T extends Set<infer E>
   ? Set<input<E>>
-  : T extends Promise<infer E>
-  ? Promise<input<E>>
+  : T extends PromiseLike<infer E>
+  ? PromiseLike<input<E>>
   : T;
-export type output<T> = T extends Transform<infer O, any>
+export type output<T> = T extends SchemaType<infer O, any>
   ? output<O>
   : T extends Date
   ? Date
@@ -30,26 +48,39 @@ export type output<T> = T extends Transform<infer O, any>
   ? Array<output<E>>
   : T extends Set<infer E>
   ? Set<output<E>>
-  : T extends Promise<infer E>
-  ? Promise<output<E>>
+  : T extends PromiseLike<infer E>
+  ? PromiseLike<output<E>>
   : T;
 
-export type TypeSchema<T> = T | Transform<T, any>;
+export type TypeSchema<Output, Input = any> =
+  | Output
+  | SchemaType<Output, Input>;
 
 export type toZod<T> = z.ZodType<output<T>, z.ZodTypeDef, input<T>>;
 
-class ParseFloat<I extends TypeSchema<string> = string> extends Transform<
-  number,
-  I
-> {
-  transform(value: input<I>): number {
+class ParseFloat<I extends TypeSchema<string>> extends Transform<number, I> {
+  transform(value: output<I>): number {
     return parseFloat(value);
   }
 }
 
-class ToString<I = unknown> extends Transform<string, I> {
-  transform(value: input<I>): string {
+class ToString<I> extends Transform<string, I> {
+  transform(value: output<I>): string {
     return String(value);
+  }
+}
+
+type HttpPath = `/${string}`;
+
+const httpPathSchema = z
+  .string()
+  .refine((value): value is HttpPath => value.startsWith("/"));
+type inp = z.input<typeof httpPathSchema>;
+type out = z.output<typeof httpPathSchema>;
+
+class ToHttpPath<I extends TypeSchema<string>> extends Refine<I, HttpPath> {
+  refine(value: output<I>): boolean {
+    return value.startsWith("/");
   }
 }
 
@@ -57,27 +88,45 @@ class ToDate<I extends TypeSchema<string | number | Date>> extends Transform<
   Date,
   I
 > {
-  transform(value: input<I>): Date {
+  transform(value: output<I>): Date {
     return new Date(value);
   }
 }
 
 class ToBigInt<
-  I extends TypeSchema<string | number | bigint | boolean> =
-    | string
-    | number
-    | bigint
-    | boolean
+  I extends TypeSchema<string | number | bigint | boolean>
 > extends Transform<bigint, I> {
-  transform(value: input<I>): bigint {
+  async transform(value: output<I>): Promise<bigint> {
     return BigInt(value);
   }
 }
 
 type Schema = {
-  a: ToBigInt<ToString<ParseFloat>> | null;
-  b?: string;
+  a: ToBigInt<ToString<ParseFloat<string>>> | null;
+  b?: ToHttpPath<ToString<number>>;
 };
+
+const parseFloatInstance = new ParseFloat();
+const toStringInstance = new ToString();
+const toBigIntInstance = new ToBigInt();
+const toHttpPathInstance = new ToHttpPath();
+
+const genZodSchema: toZod<Schema> = z.object({
+  a: z
+    .string()
+    .transform(parseFloatInstance.transform)
+    .transform(toStringInstance.transform)
+    .transform(toBigIntInstance.transform)
+    .nullable(),
+  b: z
+    .number()
+    .transform(toStringInstance.transform)
+    .transform(toHttpPathInstance.transform)
+    .optional(),
+});
+
+type ZodInput = z.input<typeof genZodSchema>;
+type ZodOutput = z.output<typeof genZodSchema>;
 
 type SchemaInput = input<Schema>;
 type SchemaOutput = output<Schema>;
