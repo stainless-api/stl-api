a4784b9deff1385d1f2c316f4fd03ef58ac6a54d
Darius Jankauskas <d5926j@gmail.com>
2023-06-29 14:41:18 -0400

    chore: add current temporary stl-api-gen test setup

--- packages/stl-api-gen/test_project/src/index.ts
+++ packages/stl-api-gen/test_project/src/index.ts
@@ -1,15 +1,49 @@
-interface Stl {
-    magic<T>(val: T): T
+import { z, Stl } from "stainless";
+import { ExternalInterface, TestEnumAdditional } from "./additional";
+import { StringSchema } from "ts-to-zod";
+import { ExternalInterface as __symbol_ExternalInterface, Test as __class_Test, InThisFile as __symbol_InThisFile, EnumTest as __enum_EnumTest } from "stl-api/gen/src/index";
+import { TestEnumAdditional as __enum_TestEnumAdditional } from "stl-api/gen/src/additional";
+const stl = new Stl({
+  plugins: {},
+});
+
+
+export enum EnumTest {
+    A,
+    B,
+    C,
+}
+// stl.magic<EnumTest>(__enum_EnumTest);
+
+export class X {}
+
+// stl.magic<X>(__class_X);
+
+stl.magic<ExternalInterface>(__symbol_ExternalInterface);
+
+export class Test {}
+
+stl.magic<Test>(__class_Test);
+
+type BadType = {
+  a: string,
+  b: [...string[], number],
 }
 
-const stl: Stl = {
-    magic<T>(val: T): T {
-        return val;
-    }
+type Mapped = Partial<{a: string}>;
+
+stl.magic<Partial<{a: string}>>(z.object({ a: z.string().optional() }));
+
+type InThisFile = {
+    id: string,
 }
 
-type T = {
-    str: string,
-}
+stl.magic<{nested: EnumTest, nested2: TestEnumAdditional}>(z.object({ nested: z.lazy(() => __enum_EnumTest), nested2: z.lazy(() => __enum_TestEnumAdditional) }));
 
-const schema = stl.magic<T>(); 
+stl.endpoint({
+  endpoint: "get /users",
+  response: stl.magic<InThisFile>(__symbol_InThisFile),
+  handler: (request, ctx) => {
+    throw new Error("dummy");
+  },
+});
