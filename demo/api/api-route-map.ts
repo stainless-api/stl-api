3c8fa7e4daa01f3171be937714448cd8bfa96eeb
Andy Edwards <jedwards@fastmail.com>
2023-07-14 13:15:15 -0500

    chore: add *-route-map.ts to prettierignore

--- demo/api/api-route-map.ts
+++ demo/api/api-route-map.ts
@@ -1,18 +1,18 @@
 import { APIRouteMap } from "stainless";
 
 export const api: APIRouteMap = {
-  actions: {
-    getOpenapi: {
-      endpoint: "get /api/openapi",
-    },
+  "actions": {
+    "getOpenapi": {
+      "endpoint": "get /api/openapi"
+    }
   },
-  namespacedResources: {
-    test: {
-      actions: {
-        foo: {
-          endpoint: "put /api/foo/{value}",
-        },
-      },
-    },
-  },
+  "namespacedResources": {
+    "test": {
+      "actions": {
+        "foo": {
+          "endpoint": "put /api/foo/{value}"
+        }
+      }
+    }
+  }
 };
