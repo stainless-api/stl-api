2f7b989b2752ccb3f8e14af56e7192a30c35d189
Matt Casey <matt@mattcasey.nyc>
2024-03-13 13:18:14 -0400

    moved test file

--- packages/client/src/core/api-client.test.ts
+++ packages/client/src/core/api-client.test.ts
@@ -1,5 +1,5 @@
 import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
-import { inferHTTPMethod, makeClient } from "./api-client";
+import { makeClient } from "./api-client";
 import { cats } from "../test-util/cat-api";
 import { dogs } from "../test-util/dog-api";
 import { dogTreats } from "../test-util/dog-treat-api";
@@ -46,54 +46,6 @@ const mockFetchImplementation = async (
 
   return new Response(JSON.stringify(payload));
 };
-
-describe("Inferring HTTP method from verb", () => {
-  it("defaults to GET", () => {
-    expect(inferHTTPMethod("foobarbaz")).toEqual("GET");
-  });
-
-  it("defaults to GET if there is no body", () => {
-    expect(inferHTTPMethod("foobarbaz", undefined)).toEqual("GET");
-  });
-
-  it("defaults to POST if there is a body", () => {
-    expect(inferHTTPMethod("foobarbaz", {})).toEqual("POST");
-  });
-
-  it("can infer GET", () => {
-    expect(inferHTTPMethod("get")).toEqual("GET");
-    expect(inferHTTPMethod("list")).toEqual("GET");
-    expect(inferHTTPMethod("retrieve")).toEqual("GET");
-  });
-
-  it("can infer POST", () => {
-    expect(inferHTTPMethod("post")).toEqual("POST");
-    expect(inferHTTPMethod("create")).toEqual("POST");
-    expect(inferHTTPMethod("make")).toEqual("POST");
-  });
-
-  it("can infer PUT", () => {
-    expect(inferHTTPMethod("put")).toEqual("PUT");
-  });
-
-  it("can infer PATCH", () => {
-    expect(inferHTTPMethod("patch")).toEqual("PATCH");
-    expect(inferHTTPMethod("update")).toEqual("PATCH");
-  });
-
-  it("can infer DELETE", () => {
-    expect(inferHTTPMethod("delete")).toEqual("DELETE");
-    expect(inferHTTPMethod("destroy")).toEqual("DELETE");
-  });
-
-  it("can infer from prefixed key word", () => {
-    expect(inferHTTPMethod("useUpdate")).toEqual("PATCH");
-  });
-
-  it("can infer from suffixed key word", () => {
-    expect(inferHTTPMethod("updateAllCats")).toEqual("PATCH");
-  });
-});
 
 describe("API Client", () => {
   describe("client configuration", () => {
