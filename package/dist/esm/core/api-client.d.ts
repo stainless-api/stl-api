827f2fd01aa22eaa16346ec0679888e6f55158ca
Matt Casey <matt@mattcasey.nyc>
2024-03-14 15:09:48 -0400

    chore: release stl-api-client-0.0.1-alpha.8.tgz [ci skip]

---
+++ package/dist/esm/core/api-client.d.ts
@@ -0,0 +1,20 @@
+import { HttpMethod } from "stainless";
+import { APIConfig, Client, ClientConfig } from "./api-client-types";
+/**
+ * Since the client needs to operate only on types,
+ * we have to guess the correct HTTP verb from the method at the end of the client API call chain
+ * @param action Client API method name, e.g. list, useRetrieve
+ * @param body Request body
+ * @returns HttpMethod
+ */
+export declare function inferHTTPMethod(action: string, body?: unknown): HttpMethod;
+/**
+ * Main entry for the client library
+ * Provides an interface to construct API calls to a server with a matching API configuration
+ * @param config
+ * @returns Client API
+ */
+export declare function makeClient<API extends APIConfig, 
+/** Unfortunately this cannot be infered from the parameter since the API generic needs to be specified */
+Config extends ClientConfig<API["basePath"]>>(config: Config): Client<API, Config>;
+//# sourceMappingURL=api-client.d.ts.map
---
+++ vitest.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    include: ["./src/**/*.{test,spec}.?(c|m)[jt]s?(x)"],
+    typecheck: { enabled: true },
+  },
+});
