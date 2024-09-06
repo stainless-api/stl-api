69f6a9a0abaa6f899f3d65d8069686caa710fdf3
David Meadows <davidtmeadows@gmail.com>
2024-09-06 14:15:28 -0400

    chore: release stl-api-client-0.0.1-alpha.32.tgz [ci skip]

---
+++ package/package/package/package/src/index.ts
@@ -0,0 +1,6 @@
+export {
+  makeClientWithInferredTypes as makeClient,
+  makeClientWithExplicitTypes,
+} from "./core/api-client";
+export { generateOutput as generateApiClient } from "./codegen/generate-types";
+export type { APIConfig, ClientConfig, Client } from "./core/api-client-types";
