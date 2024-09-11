b6a9fa9477c67212164491b41f2da1e298e1c369
David Meadows <davidtmeadows@gmail.com>
2024-09-11 10:52:25 -0400

    undo some changes

--- scripts/publish.mjs
+++ scripts/publish.mjs
@@ -10,18 +10,13 @@ await setDependencyVersions();
 
 const rootDir = path.dirname(path.dirname(url.fileURLToPath(import.meta.url)));
 
-// for now we're just updating the client package
-// const packagesDir = path.join(rootDir, "packages");
-// const packageDirs = (await fs.readdir(packagesDir)).map((dir) =>
-//   path.join(packagesDir, dir)
-// );
-
-// const dryRun = process.argv.includes("--dry-run");
-
-// for (const dir of packageDirs) {
-//   await gitPublish(dir, { dryRun });
-// }
+const packagesDir = path.join(rootDir, "packages");
+const packageDirs = (await fs.readdir(packagesDir)).map((dir) =>
+  path.join(packagesDir, dir),
+);
 
 const dryRun = process.argv.includes("--dry-run");
 
-await gitPublish(path.join(rootDir, "packages", "client"), { dryRun });
+for (const dir of packageDirs) {
+  await gitPublish(dir, { dryRun });
+}
