a13107468d2fca1f19d2f79b1afa3a1f632b9e63
Young-jin Park <youngjinpark20@gmail.com>
2025-04-29 19:08:50 -0700

    chore: release stl-api-hono-0.2.1.tgz [ci skip]

---
+++ dist/honoPlugin.d.ts
@@ -0,0 +1,16 @@
+import { Context } from "hono";
+import { AnyAPIDescription } from "stainless";
+export type HonoServerContext = {
+    type: "hono";
+    args: [Context];
+};
+declare module "stainless" {
+    interface StlContext<EC extends AnyBaseEndpoint> {
+        server: HonoServerContext;
+    }
+}
+export type StlAppOptions = {
+    handleErrors?: boolean;
+};
+export declare function stlApi({ topLevel, resources }: AnyAPIDescription, options?: StlAppOptions): import("hono").MiddlewareHandler<any, string, {}>;
+//# sourceMappingURL=honoPlugin.d.ts.map
