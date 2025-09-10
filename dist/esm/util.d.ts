2d8c19ae899af4ddcfb06ed45d7096249bd3d4c4
CJ Quines <cj@cjquines.com>
2025-09-10 19:20:32 -0400

    chore: release stl-api-react-query-0.2.4.tgz [ci skip]

---
+++ dist/esm/util.d.ts
@@ -0,0 +1,31 @@
+type ToUpperCase = {
+    a: "A";
+    b: "B";
+    c: "C";
+    d: "D";
+    e: "E";
+    f: "F";
+    g: "G";
+    h: "H";
+    i: "I";
+    j: "J";
+    k: "K";
+    l: "L";
+    m: "M";
+    n: "N";
+    o: "O";
+    p: "P";
+    q: "Q";
+    r: "R";
+    s: "S";
+    t: "T";
+    u: "U";
+    v: "V";
+    w: "W";
+    x: "X";
+    y: "Y";
+    z: "Z";
+};
+export type UpperFirst<S extends string> = S extends `${infer L extends keyof ToUpperCase}${infer Rest}` ? `${ToUpperCase[L]}${Rest}` : S;
+export {};
+//# sourceMappingURL=util.d.ts.map
