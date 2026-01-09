8a060cc94466b659fd3ff80b4ba40a65a634ac19
Benjamin Yolken <benjamin@stainless.com>
2026-01-09 16:24:42 -0500

    chore: release stl-api-client-0.2.5.tgz [ci skip]

---
+++ dist/codegen/generate-types.d.ts
@@ -0,0 +1,3 @@
+import { APIConfig, ClientConfig } from "../core/api-client-types";
+export declare function generateOutput<API extends APIConfig>(api: API, config: ClientConfig, installLocation?: string, reactQueryAlias?: string): Promise<string>;
+//# sourceMappingURL=generate-types.d.ts.map
---
+++ dist/extensions/extensions.generated.test.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=extensions.generated.test.d.ts.map
